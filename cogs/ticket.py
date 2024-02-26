a113dbfd483ea960adf98a244436af8ea9c32537
830008 <nrollins189@gmail.com>
2024-02-25 19:13:07 -0600

    Add .html to url endings

--- cogs/ticket.py
+++ cogs/ticket.py
@@ -28,7 +28,7 @@ class Ticket(Cog):
     messages = await chat_exporter.export(c)
     with open(file, "w", encoding="utf-8") as f:
       f.write(messages)
-    return f"https://logs.pretend.best/{logId}"
+    return f"https://logs.pretend.best/{logId}.html"
 
   @Cog.listener()
   async def on_guild_channel_delete(self, channel: GuildChannel):
