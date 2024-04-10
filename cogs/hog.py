ef38b4e18d0bab25acebc1e27a448f7958c99bb2
830008 <nrollins189@gmail.com>
2024-04-09 21:13:00 -0500

    more hog

---
+++ cogs/hog.py
@@ -0,0 +1,47 @@
+import os
+import psutil 
+import discord
+
+from tools.bot import Pretend 
+from tools.helpers import PretendContext
+from tools.validators import ValidCommand
+from io import BytesIO
+
+from discord import User, Embed, __version__, utils, Permissions
+from discord.ext.commands import Cog, command, hybrid_command
+from discord.ui import View, Button
+from platform import python_version
+from posthog import Posthog
+posthog = Posthog("phc_pTxc2ZEgflCq1wBWWtloXS8xqK97FPYjlpWLWseYMt8", "https://hog.semisol.dev")
+class Hog(Cog):
+  def __init__(self, bot: Pretend):
+    self.bot = bot 
+    self.description = "Posthog Analytics"
+
+  @Cog.listener
+  async def on_guild_join(self, guild: discord.Guild):
+    posthog.group_identify('guild', str(guild.id), {
+    'name': guild.name,
+    'subscription type': await get_sub_type(self, guild),
+ })
+  @Cog.listener
+  async def on_guild_update(self, before: discord.Guild, after: discord.Guild):
+        posthog.group_identify('guild', str(after.id), {
+        'name': after.name,
+        'subscription type': await get_sub_type(self, after),
+    })
+async def get_sub_type(self, guild):
+   auth = await self.bot.db.fetchrow("SELECT * FROM AUTHORIZE WHERE guild_id = $1", float(guild.id))
+   if auth:
+      till = auth.get("till")
+      if till:
+            return "monthly"
+      else:
+            return "onetime"
+   else:
+       if guild.member_count > 5000:
+          return "5k"
+       else:
+            return "none"
+async def setup(bot: Pretend) -> None: 
+  return await bot.add_cog(Hog(bot))    
