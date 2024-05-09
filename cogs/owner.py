140a021a8d1f80c9cfcf30fee93aafa33a6ddeb3
uvloop <uvloop@proton.me>
2024-05-09 11:33:35 -0500

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -305,7 +305,7 @@ class Owner(Cog):
         View information about an error code
         """
         if not ctx.author.id in self.bot.owner_ids and not ctx.author.id in (0, 732610694842810449):
-            return 
+            return await ctx.send_warning("You are not authorized to use this command.")
         fl = await self.bot.db.fetch("SELECT * FROM error_codes;")
         error_details = [x for x in fl if x.get("code") == code]
 
