8d6591d4f3b735832003781b1b2c250a4d4dbd99
830008 <nrollins189@gmail.com>
2024-05-11 14:51:58 -0500

    verification attempt 3

--- cogs/verification.py
+++ cogs/verification.py
@@ -64,7 +64,7 @@ class Verification(Cog):
             return await ctx.send("Verification is already setup in this server.")
         await self.bot.db.execute("INSERT INTO verify_guilds(guild_id, role_id) VALUES ($1, $2)", ctx.guild.id, role.id)
         embedcode = "{embed}{color: #181a14}$v{title: Verify}$v{description: Click on the button below this message to verify}$v{author: name: {guild.name} && icon: {guild.icon}}"
-        await channel.send(self.bot.embed_build.convert(ctx, embedcode), view=BtnViewStart())
+        await channel.send(await self.bot.embed_build.convert(ctx, embedcode), view=BtnViewStart())
         await ctx.send_success("Verification setup successfully.")
     @has_guild_permissions(administrator=True)    
     async def verification_reset(self, ctx: PretendContext):
