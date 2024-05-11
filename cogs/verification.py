43f8c15e8ac25d1c496148a659dccf4b87e333b7
830008 <nrollins189@gmail.com>
2024-05-11 15:14:13 -0500

    redo view again

--- cogs/verification.py
+++ cogs/verification.py
@@ -31,40 +31,36 @@ from tools.predicates import antinuke_owner, antinuke_configured, admin_antinuke
 def generate_code():
   characters = string.hexdigits.upper()
   return ''.join(random.choice(characters) if i not in (3, 6) else '-' for i in range(11))
-class BtnViewUrl(View):
-    def __init__(self, url: str):
-        super().__init__()
-        button = discord.ui.button(label="Verify", style=discord.ButtonStyle.url, url=url)
-        self.add_item(button)
 class BtnViewStart(discord.ui.View):
     def __init__(self):
         super().__init__()
 
-        # Create the button
+       
         verify_button = discord.ui.Button(label="Verify", style=discord.ButtonStyle.primary)
-        
+
 
         verify_button.callback = self.verify
-        
-     
+
+       
         self.add_item(verify_button)
-    
+
     async def verify(self, button: discord.ui.Button, interaction: discord.Interaction):
         random_alphanumeric_5 = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
         code = str(random_alphanumeric_5)
         match_code = str(generate_code())
-        
+
         await interaction.client.db.execute(
             "INSERT INTO verify_codes_discord(user_id, guild_id, guild_name, valid_until, code, match_code, confirmed) VALUES ($1, $2, $3, $4, $5, $6, $7)",
             interaction.user.id, interaction.guild.id, interaction.guild.name,
             datetime.datetime.now() + datetime.timedelta(minutes=5), code, match_code, False
         )
-        
+
         await interaction.response.send_message(
             f"Click the button below to verify {match_code}",
-            view=BtnViewUrl(f"https://pretend.bot/verify/{code}"),
+            components=self.to_components(),  # Ensure to call as a method of an instance
             ephemeral=True
         )
+
 class Verification(Cog):
     def __init__(self, bot: Pretend):
         self.bot = bot
