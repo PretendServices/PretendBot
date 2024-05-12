32cd293bd585edb27a73cd697d49e2aacda68bff
830008 <nrollins189@gmail.com>
2024-05-11 19:20:16 -0500

    attempt to fix persistence

--- tools/persistent/verification.py
+++ tools/persistent/verification.py
@@ -19,7 +19,6 @@ from discord import (
 )
 class VerificationView(discord.ui.View):
       def __init__(self):
-        self.timeout = None
         super().__init__()
         verify_button = discord.ui.Button(label="Verify", style=discord.ButtonStyle.primary, custom_id="verify:persistent")
       
